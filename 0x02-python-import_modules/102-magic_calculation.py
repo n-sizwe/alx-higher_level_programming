fa367342db62841bcd61f93e7ec23aaf713941d5
Ted Simwa <44122958+n-sizwe@users.noreply.github.com>
2024-01-24 04:43:31 +0300

    Add files via upload

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
+# 102-magic_calculation.py
+# Brennan D Baraban <375@holbertonschool.com>
+
+
 def magic_calculation(a, b):
+    """Match bytecode provided by Holberton School."""
     from magic_calculation_102 import add, sub
+
     if a < b:
-        sum = add(a, b)
+        c = add(a, b)
         for i in range(4, 6):
-            sum = add(sum, i)
-        return sum
+            c = add(c, i)
+        return (c)
+
     else:
-        return sub(a, b)
+        return(sub(a, b))
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+# 5-variable_load.py
+# Brennan D Baraban <375@holbertonschool.com>
+
 if __name__ == "__main__":
-    import variable_load_5
-    print(variable_load_5.a)
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
+
+    print(a)
