c05b093e7569808a616b4f48ebc935f37a80b7a7
Ted Simwa <44122958+n-sizwe@users.noreply.github.com>
2024-01-24 04:33:04 +0300

    Add files via upload

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for name in dir(hidden_4):
-        if name[0] != '_' and name[1] != '_':
-            print(name)
+    name = dir(hidden_4)
+    for i in range(len(name)):
+        if name[i][0] != "_" and name[i][1] != "_":
+            print(name[i])
